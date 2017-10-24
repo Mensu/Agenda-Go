af52a6ae0aac0cb5c1ce49fae0b9c98f9c74c2a0
mensu <yxshw55@qq.com>
2017-10-24 16:00:47 +0800

    为 goroutine 添加 recover

--- vendor/entity/index.go
+++ vendor/entity/index.go
@@ -32,21 +32,30 @@ func addModel(model model, filename string) {
 
 // Init initializes registered models
 func Init() {
+	var err interface{}
 	finished := make(chan bool)
 	// initialize all models concurrently
 	for _, m := range models {
 		go func(m modelConfig) {
+			defer func() {
+				if e := recover(); e != nil {
+					err = e
+				}
+				finished <- true
+			}()
 			path := viper.GetString(m.filename)
 			if len(path) == 0 {
 				os.Mkdir("data", 0755)
 				path = "data/" + m.filename + ".json"
 			}
 			m.model.Init(path)
-			finished <- true
 		}(m)
 	}
 	// wait for all models to finish initialization
 	for _ = range models {
 		<-finished
+	}
+	if err != nil {
+		logger.Fatalln(err)
 	}
 }
